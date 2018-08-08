01e35a8f8d55b5b6c08015a53168a605243a2c1b
michael <michaeldavidfarrell@gmail.com>
2018-08-07 18:16:47 -0700

    test script

---
+++ setup_system.sh
@@ -0,0 +1,35 @@
+
+# Download git, clone this repo anywhere except your home dir
+
+# Setup dot files
+git clone --bare https://github.com/mmmfarrell/dotfiles.git $HOME/.cfg
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
+
+# Install the things you need
+sudo apt-get -y install tmux vim-gtk-py2 fonts-powerline
+
+# Install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+# Install YouCompleteMe
+sudo apt-get install -y build-essential cmake
+sudo apt-get install -y python-dev python3-dev
+cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+
+# All done
+cd ~
+echo Setup Complete
+
