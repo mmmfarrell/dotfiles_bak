dd50691b662c9861159a6f0ba3d18fd0cca5ee48
Michael Farrell <michaeldavidfarrell@gmail.com>
2018-11-07 09:59:27 -0700

    install scripts

--- install_scripts/basic_setup.sh
+++ install_scripts/basic_setup.sh
@@ -17,19 +17,19 @@ fi;
 config checkout
 config config status.showUntrackedFiles no
 
-# Install the things you need
-sudo apt-get -y install tmux vim-gtk3-py2 fonts-powerline
-sudo update-alternatives --set vim /usr/bin/vim.gtk3-py2
-sudo update-alternatives --set vi /usr/bin/vim.gtk3-py2
+## Install the things you need
+#sudo apt-get -y install tmux vim-gtk3-py2 fonts-powerline
+#sudo update-alternatives --set vim /usr/bin/vim.gtk3-py2
+#sudo update-alternatives --set vi /usr/bin/vim.gtk3-py2
 
-# Install vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+## Install vim plugins
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
 
-# Install YouCompleteMe
-sudo apt-get install -y build-essential cmake
-sudo apt-get install -y python-dev python3-dev
-cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
+## Install YouCompleteMe
+#sudo apt-get install -y build-essential cmake
+#sudo apt-get install -y python-dev python3-dev
+#cd ~/.vim/bundle/YouCompleteMe && ./install.py --clang-completer
 
 # All done
 cd ~
--- install_scripts/install_nvim.sh
+++ install_scripts/install_nvim.sh
@@ -16,10 +16,28 @@ sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
 
-# xclip for clipboard
-sudo apt-get install xclip
-
+###### Plugins #####
 # Vim-plug
 sudo apt-get install curl
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# xclip for clipboard
+sudo apt-get install xclip
+
+# powerline fonts
+sudo apt-get install fonts-powerline
+
+# LSP
+# Python pyls
+pip install python-language-server
+
+# C/C++ cquery
+mkdir -p ~/installed_apps
+cd ~/installed_apps
+git clone --recursive https://github.com/cquery-project/cquery.git
+cd cquery
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/home/mmmfarrell/.local/ -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
+cmake --build .
+cmake --build . --target install
